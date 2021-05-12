6d6327edaaae33b165e0686e8af61c66089c84fd
Dobbs <stevedobbs2@byui.edu>
2021-05-12 17:25:00 -0400

    Added a line to the code.

--- src/Bacilio/Main.java
+++ src/Bacilio/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Ricardo");
+        System.out.println("Hello Ricardo, my name is Steve.");
     }
 }
